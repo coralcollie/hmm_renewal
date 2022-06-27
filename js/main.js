a8d126183028b9bbc72056fd21e945e9628c85e8
coralcollie <coralcollie@gmail.com>
2022-06-27 15:35:13 +0900

    라이브러리 추가 서브페이지- 서비스 생성

--- js/main.js
+++ js/main.js
@@ -3,16 +3,12 @@
 /* -----------cursor style-------------- */
 
 let cursor = document.getElementById("cursor");
-// let aTag = document.getElementsByTagName("a")
 
 document.onmousemove = function(e){
     cursor.style.left = (e.pageX) + "px";
     cursor.style.top = (e.pageY)+ "px";
     cursor.style.display = "block";
 
-    // aTag.addEventListener("mouseenter", function(){
-    //     cursor.style.background = "transparent";
-    // })
 }
 
 
@@ -68,6 +64,8 @@ moveSlide(0);
 
 /* -----------SCROLL ANIMATION-------------- */
 window.addEventListener('scroll',()=>{
+
+    /* -------------header animation-------------- */ 
     let winSCT = window.scrollY;
     let header = document.querySelector('header');
 
@@ -78,30 +76,61 @@ window.addEventListener('scroll',()=>{
         header.classList.remove('active');
     }
     
-    /* -------------service animation-------------- */ 
+    /* -------------section animation-------------- */ 
     let service = document.querySelector('.service'),
-        serviceUl = service.querySelector('ul')
-        serviceOST = service.offsetTop-737;
+        serviceUl = service.querySelector('ul'),
+        serviceOST = service.offsetTop-537;
 
-    console.log(serviceOST);
-    console.log(winSCT);
-    if(winSCT>=serviceOST){
-        service.classList.add('animate__fadeInUp');
-        serviceUl.classList.add('animate__fadeInUp');
-    }else{
-        service.classList.remove('animate__fadeInUp');
-        serviceUl.classList.remove('animate__fadeInUp');
+    if(winSCT>serviceOST){
+        service.classList.add('animate__animated','animate__backInUp');
+        serviceUl.classList.add('animate__animated','animate__backInUp');
     }
+
+    let worksNews = document.querySelector('.works_news'),
+        worksNewsF = worksNews.querySelector('li:nth-child(1)'),
+        worksNewsS = worksNews.querySelector('li:nth-child(2)'),
+        worksNewsT = worksNews.querySelector('li:nth-child(3)'),
+        worksNewsOST = worksNews.offsetTop-549;
+    if(winSCT>worksNewsOST){
+        worksNewsF.classList.add('animate__animated','animate__fadeInRight');
+    }
+    if(winSCT>worksNewsOST+339){
+        worksNewsS.classList.add('animate__animated','animate__fadeInLeft');
+    }
+    if(winSCT>worksNewsOST+741){
+        worksNewsT.classList.add('animate__animated','animate__fadeInRight');
+    }
+
+    let result = document.querySelector('.result'),
+        resultTit = document.querySelector('.result_tit'),
+        resultWrap = document.querySelector('.result .content_wrap'),
+        resultOST = result.offsetTop-506;
+    if(winSCT>resultOST){
+        resultTit.classList.add('animate__animated','animate__fadeInUp');
+        resultWrap.classList.add('animate__animated','animate__flipInX');
+    }
+
+    let gallery = document.querySelector('.gallery'),
+        galleryList = document.querySelector('.gallery_list'),
+        galleryOST = gallery.offsetTop-507;
+    if(winSCT>galleryOST){
+        gallery.classList.add('animate__animated','animate__backInUp');
+        galleryList.classList.add('animate__animated','animate__backInUp');
+    }
+
+    // console.log(winSCT,galleryOST);
+
+
     
     /* -------------Counter animation-------------- */ 
     let counters = document.querySelector('.content_wrap');
     let counterNums = counters.querySelectorAll('.content h3');
     let courtersOST = counters.offsetTop - 700;
-    let excuted = false;
+    let execute = false;
 
 
-    if(winSCT>courtersOST){
-        if(!excuted){
+    if(winSCT>=courtersOST){
+        if(!execute){
             counterNums.forEach(item=>{
                 let targetNum = item.getAttribute('data-target');
                 let speed = 4;
@@ -124,9 +153,8 @@ window.addEventListener('scroll',()=>{
                         item.innerText =  num;
                         
                     }, speed);                 
-                    
             });
-            excuted = true;
+            execute = true;
         }
     }
 
@@ -148,3 +176,7 @@ var mixer = mixitup('.gallery_list',{
     }
 });
 
+/* -----------banner slide-------------- */
+$('.banner_item').slick({
+
+});
